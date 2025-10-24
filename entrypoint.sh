afe073bced7961033a150f6ac4effc07d6df3725
黄炳华 <em17817344484@gmail.com>
2025-10-24 15:00:30 +0800

    数据保留

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # 1. Run database migrations and seeding
 echo "Running database push and seed..."
-npx prisma db push --force-reset
+npx prisma db push
 node dist/prisma/seed.js
 
 # 2. Start the application using PM2 in cluster mode
